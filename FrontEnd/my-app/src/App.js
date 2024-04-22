83ffa51a65af9cd97357a7cd7c52e3e2961eef4a
joel olivero <joelolivero@outlook.com>
2024-04-22 12:29:37 -0400

    added Landing page img and texted centered

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import logo from './podcast.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <header className="App-header"></header>
+
+      <div className="bg">
+        <h1>Welcome to <span> Lotus</span></h1>
+
+       </div>
+
+       <div class="login-html">
+         <input id="tab-1" type="radio" name="tab" class="sign-in" ><label for="tab-1" class="tab"/>Sign In</label>
+         <input id="tab-2" type="radio" name="tab" class="sign-up"GI><label for="tab-2" class="tab"/>Sign Up</label>
+         <div class="login-form">
+           <div class="sign-in-htm">
+
+
+
     </div>
   );
 }
